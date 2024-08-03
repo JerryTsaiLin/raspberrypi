2fb82e8d10ca13e5d9e1eb30bf47c6c8513e5358
jerry.tsailin@gmail.com <jerry.tsailin@gmail.com>
2024-08-03 10:59:10 +0800

    update

--- lession080301.py
+++ lession080301.py
@@ -2,17 +2,17 @@ import os.path
 import random
 from datetime import datetime
 
-def created_logfile():
+def created_logfile(folder:str,file:str)->str:
     current_path=os.path.abspath(__name__)#取得目前檔案路徑
     directory_name=os.path.dirname(current_path) #取得目前目錄名稱
-    data_path=os.path.join(directory_name,'data')#目前資料夾路徑上加上data目錄
+    data_path=os.path.join(directory_name,folder)#目前資料夾路徑上加上data目錄
     if not os.path.isdir(data_path):
         print("沒有data的目錄,手動建立目錄")
         os.mkdir(data_path)
     else:
         print("目錄已建立!") 
 
-    log_path=os.path.join(data_path,'iot.log')
+    log_path=os.path.join(data_path,file)
 
     if not os.path.isfile(log_path):
         with open(log_path,mode='w',encoding='utf-8',newline=None) as file:
@@ -39,7 +39,7 @@ def record_info(log_path):
 
 
 def main():
-    log_path=created_logfile()
+    log_path=created_logfile(folder='data',file='iot.log')
 
     record_info(log_path)
    
